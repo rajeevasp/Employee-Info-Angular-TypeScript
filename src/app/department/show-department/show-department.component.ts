c46b4fb5cb821a508c082992e417da5efdb95866
Ram <ramavtar1988@gmail.com>
2023-05-24 07:36:10 +0530

    Add project files.

---
+++ src/app/department/show-department/show-department.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiserviceService } from 'src/app/apiservice.service';
+
+@Component({
+  selector: 'app-show-department',
+  templateUrl: './show-department.component.html',
+  styleUrls: ['./show-department.component.css']
+})
+export class ShowDepartmentComponent  implements OnInit{
+  constructor(private service: ApiserviceService) { }
+
+  DepartmentList: any = [];
+  ModalTitle = "";
+  ActivateAddEditDepartComp: boolean = false;
+  depart: any;
+
+  DepartmentIdFilter = "";
+  DepartmentNameFilter = "";
+  DepartmentListWithoutFilter: any = [];
+
+  ngOnInit(): void {
+    this.refreshDepList();
+  }
+  
+
+  addClick() {
+    this.depart = {
+      DepartmentId: "0",
+      DepartmentName: ""
+    }
+    this.ModalTitle = "Add Department";
+    this.ActivateAddEditDepartComp = true;
+  }
+
+  closeClick() {
+    this.ActivateAddEditDepartComp = false;
+    this.refreshDepList();
+  }
+
+  editClick(item: any) {
+    this.depart = item;
+    this.ModalTitle = "Edit Department";
+    this.ActivateAddEditDepartComp = true;
+  }
+
+  deleteClick(item: any) {
+      if (confirm('Are you sure??')) {
+      this.service.deleteDepartment(item.DepartmentId).subscribe(data => {
+        alert(data.toString());
+        this.refreshDepList();
+      })
+    }
+  }
+
+  refreshDepList() {
+    this.service.getDepartmentList().subscribe(data => {
+      this.DepartmentList = data;
+      //this.DepartmentListWithoutFilter = data;
+    });
+  }
+}
